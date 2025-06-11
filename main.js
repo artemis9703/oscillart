5d61148065b3023888a3018a6840a8b71bf14e85
Art3mis <pyatt.eve@gmail.com>
2025-06-11 12:19:03 +0000

    im stuck :(

--- main.js
+++ main.js
@@ -11,13 +11,20 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
+var interval = null;
+var amplitude = 40;
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
 notes = new Map();
 notes.set("C", 261.6);
 notes.set("D", 293.7);
 notes.set("E", 329.6);
 notes.set("F", 349.2);
 notes.set("G", 392.0);
-notes.set("A", 440.0);
+notes.set("A", 440);
 notes.set("B", 493.9);
 
 function frequency(pitch) {
@@ -30,4 +37,28 @@ function handle() {
     audioCtx.resume();
     gainNode.gain.value = 0;
     frequency(input.value); 
+    var pplsNotes = String(input.value);
+    frequency(notes.get(pplsNotes))
+    drawWave();
+}
+var counter = 0;
+function drawWave() {
+    ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height/2;
+    ctx.moveTo(x,y);
+    ctx.beginPath();
+    counter = 0;
+    interval = setInterval(line, 20);
+}
+function line() {
+    freq = pitch / 10000;
+    y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    x = x + 1;
+    counter++;
+    if(counter > 50) {
+        clearInterval(interval);
+    }
 }
